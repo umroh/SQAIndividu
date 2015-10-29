533a18a0dd6f34a0a4e2d42f30fd56e8f5e55a7a
umroh <umrohmachfudza@gmail.com>
2015-10-29 01:31:02 +0000

    ch04 : still on refactoring things, it's not worked yet, I think

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 
 def home_page(request):
-	return HttpResponse('<html><title>To-Do lists</title><h1>Umroh Machfudza Sihaloho\'s To-Do List</h1></html>')	
+	return render(request, 'home.html')
+#	return HttpResponse('<html><title>To-Do</title><h1>Umroh Machfudza Sihaloho\'s To-Do List</h1></html>')	
 
