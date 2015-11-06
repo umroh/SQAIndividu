f95eabe5c7a8134dfff99185e529a1d3dddc9f89
umroh <umrohmachfudza@gmail.com>
2015-11-06 11:19:13 +0000

    delete mypmpl in repo

--- lists/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-#from django.http import HttpResponse
-
-# Create your views here.
-
-def home_page(request):
-	return render(request, 'home.html')
-#	return HttpResponse('<html><title>To-Do list<title><h1>Umroh Machfudza Sihaloho\'s To-Do List</h1></html>')	
-
