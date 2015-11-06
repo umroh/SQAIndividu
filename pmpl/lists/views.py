01c600efecd42ed92c0e7d4753a34e4ab2b7815e
umroh <umrohmachfudza@gmail.com>
2015-11-06 07:49:58 +0000

    Ch5 finish

--- pmpl/lists/views.py
+++ pmpl/lists/views.py
@@ -11,6 +11,6 @@ def home_page(request):
 		Item.objects.create(text = request.POST['item_text'])
 		return redirect('/')
 		
-		
-	return render(request, 'home.html')
+	items = Item.objects.all()	
+	return render(request, 'home.html', {'items' : items})
 
